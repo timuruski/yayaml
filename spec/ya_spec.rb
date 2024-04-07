ea629fd1500442a012b53cacd200682b3352ede7
Tim Uruski <hey@tim.uru.ski>
2024-04-07 15:10:28 -0600

    Move bin/ya to exe/ya so gem builds properly

--- spec/ya_spec.rb
+++ spec/ya_spec.rb
@@ -1,15 +1,15 @@
 require "spec_helper"
 
-RSpec.describe "bin/ya" do
+RSpec.describe "exe/ya" do
   it "searches for YAML values for a pattern" do
-    expect(`bin/ya Hello spec/fixtures`)
+    expect(`exe/ya Hello spec/fixtures`)
       .to eq(<<~EOS)
         spec/fixtures/en-US.yml:4 en-US.app.greeting: Hello, world!
       EOS
   end
 
   it "searches for YAML paths for a pattern" do
-    expect(`bin/ya -p greeting spec/fixtures`)
+    expect(`exe/ya -p greeting spec/fixtures`)
       .to eq(<<~EOS)
         spec/fixtures/en-US.yml:4 en-US.app.greeting: Hello, world!
         spec/fixtures/fr-FR.yml:4 fr-FR.app.greeting: Bonjour le monde!
