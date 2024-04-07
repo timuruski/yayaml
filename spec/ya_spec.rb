6c22e015c84b406898a53a8d879cab7f92dcef6c
Tim Uruski <hey@tim.uru.ski>
2024-04-07 15:47:32 -0600

    Warn on YAML parsing errors

--- spec/ya_spec.rb
+++ spec/ya_spec.rb
@@ -2,17 +2,24 @@ require "spec_helper"
 
 RSpec.describe "exe/ya" do
   it "searches for YAML values for a pattern" do
-    expect(`exe/ya Hello spec/fixtures`)
-      .to eq(<<~EOS)
+    expect { system("exe/ya Hello spec/fixtures") }
+      .to output(<<~EOS).to_stdout_from_any_process
         spec/fixtures/en-US.yml:4 en-US.app.greeting: Hello, world!
       EOS
   end
 
   it "searches for YAML paths for a pattern" do
-    expect(`exe/ya -p greeting spec/fixtures`)
-      .to eq(<<~EOS)
+    expect { system("exe/ya -p greeting spec/fixtures") }
+      .to output(<<~EOS).to_stdout_from_any_process
         spec/fixtures/en-US.yml:4 en-US.app.greeting: Hello, world!
         spec/fixtures/fr-FR.yml:4 fr-FR.app.greeting: Bonjour le monde!
+      EOS
+  end
+
+  it "raises an error if the file isn't valid YAML" do
+    expect { system("exe/ya -p greeting spec/spec_helper.rb") }
+      .to output(<<~EOS).to_stderr_from_any_process
+        Failed to parse YAML spec/spec_helper.rb:1:0
       EOS
   end
 end
